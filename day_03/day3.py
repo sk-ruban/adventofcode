6ef098da194915792573e06300f35647f61dfcf3
Ruban <65346010+sk-ruban@users.noreply.github.com>
2021-12-03 23:29:00 +0800

    adding day3 part2

--- day_03/day3.py
+++ day_03/day3.py
@@ -2,29 +2,75 @@ report = [x.strip() for x in open('input')]
 #report = ['00100', '11110', '10110', '10111', '10101', '01111', '00111', '11100', '10000', '11001', '00010', '01010']
 
 # Part 1
-counter_0 = 0
-counter_1 = 0
-gamma_rate = ''
-epsilon_rate = ''
+gamma = ''
+epsilon = ''
+reportLen = len(report[0])
 
-for digit in range(len(report[0])):
+for digit in range(reportLen):
+    counter_0 = 0
+    counter_1 = 0
     for each in report:
         if int(each[digit]) == 1:
             counter_1 += 1
-        if int(each[digit]) == 0:
+        else:
             counter_0 += 1
     if counter_1 > counter_0:
-        gamma_rate += '1'
-        epsilon_rate += '0'
+        gamma += '1'
+        epsilon += '0'
     else:
-        gamma_rate += '0'
-        epsilon_rate += '1'
-    counter_0 = 0
-    counter_1 = 0
+        gamma += '0'
+        epsilon += '1'
 
-power_consumption = int(gamma_rate,2) * int(epsilon_rate,2)
+power_consumption = int(gamma, 2) * int(epsilon, 2)
 print(power_consumption)
 
 # Part 2
+o2List = report[:]
+co2List = report[:]
 
-#life_support = O2_generator * CO2_scrubber
+for digit in range(reportLen):
+    if len(co2List) > 1:
+        counter_0 = 0
+        counter_1 = 0
+        tempList = []
+        for each in o2List:
+            if int(each[digit]) == 1:
+                counter_1 += 1
+            else:
+                counter_0 += 1
+        if counter_0 > counter_1:
+            for i in o2List:
+                if i[digit] == '0':
+                    tempList.append(i)
+        else:
+            for i in o2List:
+                if i[digit] == '1':
+                    tempList.append(i)
+        o2List = tempList[:]
+    else:
+        break
+
+for digit in range(reportLen):
+    if len(co2List) > 1:
+        counter_0 = 0
+        counter_1 = 0
+        tempList = []
+        for each in co2List:
+            if int(each[digit]) == 1:
+                counter_1 += 1
+            else:
+                counter_0 += 1
+        if counter_1 < counter_0:
+            for i in co2List:
+                if i[digit] == '1':
+                    tempList.append(i)
+        else:
+            for i in co2List:
+                if i[digit] == '0':
+                    tempList.append(i)
+        co2List = tempList[:]
+    else:
+        break
+
+life_support = int(o2List[0], 2) * int(co2List[0], 2)
+print(life_support)
