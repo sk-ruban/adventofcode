074ed7198c2c87dc40f109d57dab1845f9f01751
Ruban <65346010+sk-ruban@users.noreply.github.com>
2022-12-08 17:14:17 +0800

    day08 - washed

--- 2022/day08 - treetop tree house/day08.py
+++ 2022/day08 - treetop tree house/day08.py
@@ -1,70 +1,53 @@
 """
 How many trees are visible from outside the grid?
 """
-# Unwashed af
 
 
-def max_height(tree, top, bottom, left, right):
-    max_top = int(max(top, default=0))
-    max_bottom = int(max(bottom, default=0))
-    max_left = int(max(left, default=0))
-    max_right = int(max(right, default=0))
-    if (int(tree) > max_top) or (int(tree) > max_bottom) or (int(tree) > max_right) or (int(tree) > max_left):
+def max_height(tree_height, t, b, l, r):
+    max_top = int(max(t, default=0))
+    max_bottom = int(max(b, default=0))
+    max_left = int(max(l, default=0))
+    max_right = int(max(r, default=0))
+    if any(tree_height > max for max in [max_top, max_bottom, max_left, max_right]):
         return 1
-    elif int(tree) == 0 and (len(top) == 0 or len(bottom) == 0 or len(left) == 0 or len(right) == 0):
+    elif tree == 0 and 0 in (len(t), len(b), len(l), len(r)):
         return 1
     else:
         return 0
 
 
-def scenic(tree, top, bottom, left, right):
-    top_points = 0
-    bottom_points = 0
-    left_points = 0
-    right_points = 0
-    tree = int(tree)
-    for each in reversed(top):
+def direction_points(tree, direction):
+    points = 0
+    for each in direction:
         if tree <= int(each):
-            top_points += 1
+            points += 1
             break
         else:
-            top_points += 1
-    for each in bottom:
-        if tree <= int(each):
-            bottom_points += 1
-            break
-        else:
-            bottom_points += 1
-    for each in reversed(left):
-        if tree <= int(each):
-            left_points += 1
-            break
-        else:
-            left_points += 1
-    for each in right:
-        if tree <= int(each):
-            right_points += 1
-            break
-        else:
-            right_points += 1
-    scenic_score = top_points * bottom_points * right_points * left_points
-    return scenic_score
+            points += 1
+    return points
+
+
+def scenic(tree, t, b, l, r):
+    top_points = direction_points(tree, reversed(t))
+    bottom_points = direction_points(tree, b)
+    left_points = direction_points(tree, reversed(l))
+    right_points = direction_points(tree, r)
+    return top_points * bottom_points * right_points * left_points
 
 
 grid = [x.strip() for x in open('input')]
-length = len(grid)
 silver = 0
 gold = 0
 for row, trees in enumerate(grid):
     for col, tree in enumerate(trees):
         top = [x[col] for x in grid[0:row]]
-        bottom = [x[col] for x in grid[row+1:length]]
+        bottom = [x[col] for x in grid[row+1:len(grid)]]
         left = [x for x in grid[row]][0:col]
-        right = [x for x in grid[row]][col+1:length]
-        silver += max_height(tree, top, bottom, left, right)
-        gold = max(gold, scenic(tree, top, bottom, left, right))
+        right = [x for x in grid[row]][col+1:len(grid)]
+        silver += max_height(int(tree), top, bottom, left, right)
+        gold = max(gold, scenic(int(tree), top, bottom, left, right))
 
-print(silver)
-print(gold)
+print(f"silver: {silver}")
+print(f"gold: {gold}")
 
 
