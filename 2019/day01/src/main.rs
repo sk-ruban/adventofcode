802940c13436cf0370c85a2710ec8db721106884
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-01-25 16:42:00 +0800

    Add 2019 day 01 part 2

--- 2019/day01/src/main.rs
+++ 2019/day01/src/main.rs
@@ -3,11 +3,24 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("src/input")
         .expect("File cannot be read");
+
     let mut part1 = 0;
-    for line in input.lines() {
-        let mass: i32 = line.parse().unwrap();
-        part1 += mass / 3 - 2;
+    let mut part2 = 0;
+
+    for mass in input.lines() {
+        let mut fuel = compute_fuel(mass.parse().unwrap());
+        part1 += fuel;
+
+        while fuel > 0 {
+            part2 += fuel;
+            fuel = compute_fuel(fuel);
+        }
     }
+
     println!("{}", part1); // 3337766
+    println!("{}", part2); // 5003788
 }
 
+fn compute_fuel(mass: i32) -> i32 {
+    mass / 3 - 2
+}
