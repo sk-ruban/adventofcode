68ab6147684eec6c323cc57ea710e6037e0cf11d
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-01-28 15:27:40 +0800

    Add 2020 day 01 part 2

--- 2020/day01/main.swift
+++ 2020/day01/main.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 let filePath = "2020/day01/input" 
+var part1: Int = 0
+var part2: Int = 0
 
 do {
     let content = try String(contentsOfFile: filePath, encoding: .utf8)
@@ -11,7 +13,13 @@ do {
             let a = Int(a) ?? 0
             let b = Int(b) ?? 0
             if a + b == 2020 {
-                print("Part 1: \(a) * \(b) = \(a*b)") // 1007104
+                part1 = a*b 
+            }
+            for c in lines {
+                let c = Int(c) ?? 0
+                if a + b + c == 2020 {
+                    part2 = a*b*c
+                }
             }
         }
     }
@@ -19,3 +27,6 @@ do {
 } catch {
     print("Could not read the file: \(error)")
 }
+
+print("Part 1: \(part1)") // 1007104
+print("Part 2: \(part2)") // 18847752
