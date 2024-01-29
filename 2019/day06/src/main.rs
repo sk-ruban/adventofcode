37a9981dc9d28e88f718e85f1e57b6382339b40a
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-01-29 21:56:58 +0800

    Add 2019 day 06 part 2

--- 2019/day06/src/main.rs
+++ 2019/day06/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 const INPUT: &str = include_str!("input");
 
@@ -20,5 +20,23 @@ fn main() {
         }
     }
 
-    println!("Part 1: {}", count) // 227612
+    let mut from_start= HashSet::new();
+    let mut start = "YOU";
+    while let Some(direct_parent) = orbits.get(start) {
+        start = direct_parent;
+        from_start.insert(start);
+    }
+
+    let mut to_end = HashSet::new();
+    let mut end = "SAN";
+    while let Some(direct_parent) = orbits.get(end) {
+        end = direct_parent;
+        to_end.insert(end);
+    }
+
+    let journey1 = from_start.difference(&to_end);
+    let journey2 = to_end.difference(&from_start);
+
+    println!("Part 1: {}", count); // 227612
+    println!("Part 2: {}", journey1.count() + journey2.count()); // 454
 }
