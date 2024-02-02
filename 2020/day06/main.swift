267140c0abf10d9b7af18f5bf071e898a5bc87ae
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-02-02 15:26:33 +0800

    Add 2020 day 06 part 2

--- 2020/day06/main.swift
+++ 2020/day06/main.swift
@@ -5,15 +5,23 @@ let content = try String(contentsOfFile: filePath, encoding: .utf8)
 let list = content.split(separator: "\n\n")
 
 var part1 = 0
+var part2 = 0
 
 for group in list {
-    var yes = Set<Character>()
+    var anyone_yes = Set<Character>()
+    var everyone_yes: [Character: Int] = [:]
+    var people = 0
+
     for person in group.split(separator: "\n") {
         for answer in person {
-            yes.insert(answer)
+            anyone_yes.insert(answer)
+            everyone_yes[answer, default: 0] += 1
         }
+        people += 1
     }
-    part1 += yes.count
+    part1 += anyone_yes.count
+    part2 += everyone_yes.filter { $0.value == people }.count
 }
 
-print("Part 1: \(part1)")
+print("Part 1: \(part1)") // 6911
+print("Part 2: \(part2)") // 3473
