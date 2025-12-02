d4263bfb0670b20d611a69b6bcfa40eeeba328fe
Ruban <65346010+sk-ruban@users.noreply.github.com>
2025-12-02 13:50:35 +0800

    solve: 2025 day 02 part 2

--- 2025/day02/day02.py
+++ 2025/day02/day02.py
@@ -2,12 +2,14 @@ ranges = [(int(a), int(b))
     for pair in open('input').read().strip().split(",")
     for a, b in [pair.split('-')]]
 part1 = 0
+part2 = 0
 
-for id in ranges:
-    first, last = id[0], id[1]
+for first, last in ranges:
     for i in range(first, last+1):
         id = str(i)
         if len(id) % 2 == 0 and id[:len(id)//2] == id[len(id)//2:]:
             part1 += i
+        if id in (id + id)[1:-1]:
+            part2 += i
 
-print(part1)
+print(part1, part2)
