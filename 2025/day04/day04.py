0cb6a95a0d4675dfdd7d65f7ae92849dbc53d74f
Ruban <65346010+sk-ruban@users.noreply.github.com>
2025-12-04 18:47:20 +0800

    solve: 2025 day04 part 2

--- 2025/day04/day04.py
+++ 2025/day04/day04.py
@@ -1,23 +1,33 @@
-grid = [x.strip() for x in open('input')]
-part1 = 0
+grid = [list(x.strip()) for x in open('input')]
+part1, part2 = 0, 0
 
 rows, cols = len(grid), len(grid[0])
+changed = True
 
-for x in range(cols):
-    for y in range(rows):
-        if grid[x][y] != '@':
-            continue
+def count(r, c):
+    neighbours = 0
+    for dr in [-1, 0, 1]:
+        for dc in [-1, 0, 1]:
+            if dr == 0 and dc == 0:
+                continue
+            nr, nc = r + dr, c + dc
+            if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == '@':
+                    neighbours += 1
 
-        neighbours = 0
-        for dx in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
-                if dy == 0 and dx == 0:
-                    continue
-                ny, nx = y + dy, x + dx
-                if 0 <= ny < rows and 0 <= nx < cols and grid[nx][ny] == '@':
-                        neighbours += 1
+    return neighbours
 
-        if neighbours < 4:
+for r in range(rows):
+    for c in range(cols):
+        if grid[r][c] == '@' and count(r, c) < 4:
             part1 += 1
 
-print(part1)
+while changed:
+    changed = False
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == '@' and count(r, c) < 4:
+                part2 += 1
+                grid[r][c] = 'x'
+                changed = True
+
+print(part1, part2)
