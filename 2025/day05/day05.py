fc3df2cdfb318e13dc83f25737c64e8e6819e188
Ruban <65346010+sk-ruban@users.noreply.github.com>
2025-12-05 18:27:26 +0800

    solve: 2025 day05 part 2

--- 2025/day05/day05.py
+++ 2025/day05/day05.py
@@ -11,4 +11,15 @@ def fresh(x):
 
 part1 = sum(fresh(id) for id in ingredients)
 
-print(part1)
+freshList.sort()
+combined = []
+
+for lo, hi in freshList:
+    if combined and lo <= combined[-1][1]:
+        combined[-1] = (combined[-1][0], max(combined[-1][1], hi))
+    else:
+        combined.append((lo, hi))
+
+part2 = sum(hi - lo + 1 for lo, hi in combined)
+
+print(part1, part2)
