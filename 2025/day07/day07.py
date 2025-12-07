449aa2d1e3542c800d1d25bbe1cdcc486879cdb7
Ruban <65346010+sk-ruban@users.noreply.github.com>
2025-12-07 18:01:16 +0800

    solve: 2025 day07 part 1

---
+++ 2025/day07/day07.py
@@ -0,0 +1,16 @@
+diagram = open("input").read().splitlines()
+
+part1 = 0
+rows, cols = len(diagram), len(diagram[-1])
+beams = [0] * cols
+beams[(cols // 2)] = 1
+
+for r in range(rows):
+    for c in range(cols):
+        if diagram[r][c] == '^' and beams[c] and 0 < c < cols:
+            beams[c+1] += 1
+            beams[c-1] += 1
+            beams[c] = 0
+            part1 += 1
+
+print(part1)
