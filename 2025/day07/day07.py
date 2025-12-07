61199e977a0b774590f34bb560a1533acef2114b
Ruban <65346010+sk-ruban@users.noreply.github.com>
2025-12-07 18:02:47 +0800

    solve: 2025 day07 part 2

--- 2025/day07/day07.py
+++ 2025/day07/day07.py
@@ -8,9 +8,10 @@ beams[(cols // 2)] = 1
 for r in range(rows):
     for c in range(cols):
         if diagram[r][c] == '^' and beams[c] and 0 < c < cols:
-            beams[c+1] += 1
-            beams[c-1] += 1
+            beams[c+1] += beams[c]
+            beams[c-1] += beams[c]
             beams[c] = 0
             part1 += 1
 
-print(part1)
+part2 = sum(beams)
+print(part1, part2)
