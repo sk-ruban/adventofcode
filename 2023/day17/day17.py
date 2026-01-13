c59103c411ff2ef070bc37be0aba1424eba2cc23
Ruban <65346010+sk-ruban@users.noreply.github.com>
2026-01-13 18:56:01 +0800

    solve: 2023 day17 part 2

--- 2023/day17/day17.py
+++ 2023/day17/day17.py
@@ -5,32 +5,34 @@ grid = {complex(i, j): int(c)
     for j, r in enumerate(open("input").read().splitlines())
     for i, c in enumerate(r)
 }
+goal = [*grid][-1]
 
-tiebreaker = count()
-pq = [(0, next(tiebreaker), 0, 1, 0), (0, next(tiebreaker), 0, 1j, 0)]
-seen = set()
+def find_heat_loss(min, max):
+    tiebreaker = count()
+    pq = [(0, next(tiebreaker), 0, 1, 0), (0, next(tiebreaker), 0, 1j, 0)]
+    seen = set()
 
-def find_heat_loss():
     while pq:
         cost, _, pos, d, steps = heapq.heappop(pq)
         if (pos, d, steps) in seen:
             continue
         seen.add((pos, d, steps))
-        if pos == [*grid][-1]:
+        if pos == goal and steps >= min:
             return cost
         for new_d in [d, d * 1j, d * -1j]:
             new_pos = pos + new_d
             if new_pos not in grid:
                 continue
 
-            if new_d == d and steps < 3:
+            if new_d == d and steps < max:
                 new_steps = steps + 1
-            elif new_d != d:
+            elif new_d != d and min <= steps:
                 new_steps = 1
             else:
                 continue
 
             heapq.heappush(pq, (cost + grid[new_pos], next(tiebreaker), pos + new_d, new_d, new_steps))
 
-part1 = find_heat_loss()
-print(part1)
+part1 = find_heat_loss(1, 3)
+part2 = find_heat_loss(4, 10)
+print(part1, part2)
